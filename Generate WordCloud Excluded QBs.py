9f004fc210c160d1a06f82b8bc7287dc4fa51fb6
rickscole <ricksaubercole@gmail.com>
2025-06-06 09:34:59 -0400

    Add files via upload

---
+++ Generate WordCloud Excluded QBs.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  6 09:21:46 2025
+
+@author: rsc
+
+PURPOSE:
+    Generate a WordCloud from a CSV, and save to image
+
+NOTES:
+    202506050949
+    Apart of the NFL QBR Failure to Launch story (schema: qbrftl)
+    
+    202506050950
+    The native separator for the wordcloud is the space, so first we replace the space with "_".
+    Later, we inject the space back in.
+"""
+
+
+#%% IMPORT LIBARIES
+
+
+## import libraries
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import random
+
+
+#%% DEFINE FUNCTIONS
+
+
+# create a custom color function
+def random_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+    return random.choice(colors)
+
+
+#%% READ DATA
+
+
+
+# variables
+csv_file_path = r'/Users/rcole/Desktop/imacgration/lfn/qbrftl nfl excluded qb.csv'
+text_column_name = 'name_display'
+count_column = 'the_count'
+colors = ['#c60c30', '#f43e63', '#600618', '#eb5b38', '#2f3647']
+output_file_path = r'/Users/rcole/Desktop/imacgration/lfn/qbrftl nfl excluded qb.png'
+
+
+# load the CSV
+df = pd.read_csv(csv_file_path)
+df['full_name_fixed'] = df[text_column_name].astype(str).str.replace(' ', '_') #202506050950
+
+
+#%% CREATE WORDCLOUD
+
+
+# combine all text from the column into a single string
+text = ' '.join(df['full_name_fixed'].astype(str).tolist())
+
+# create the word cloud
+wordcloud = WordCloud(width=800, height=400, background_color='white', collocations=False).generate(text) #create wordcloud
+frequencies = dict(zip(df['full_name_fixed'], df[count_column])) #create dictionary object
+frequencies_display = {word.replace('_', ' '): freq for word, freq in frequencies.items()} #202506050950
+final_wordcloud = WordCloud(width=800, height=400, background_color='white', collocations=False).generate_from_frequencies(frequencies_display)
+final_wordcloud = final_wordcloud.recolor(color_func=random_color_func) #randomly assign colors from the color palette
+
+
+#%% SAVE IMAGE
+
+
+# save the word cloud to a file
+final_wordcloud.to_file(output_file_path)
