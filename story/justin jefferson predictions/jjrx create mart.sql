b4bcc664f28fe85e71e75699d1eaf94ac012d040
rickscole <ricksaubercole@gmail.com>
2025-06-30 09:31:16 -0400

    Update jjrx create mart.sql
    Add in PFR Game ID
    Change output table name to remove "JJ"

--- story/justin jefferson predictions/jjrx create mart.sql
+++ story/justin jefferson predictions/jjrx create mart.sql
@@ -1,4 +1,8 @@
-create procedure jjprx.nfl_create_mart as 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+alter procedure [jjprx].[nfl_create_mart] as 
 
 -- predrop tables
 if object_id('temp.nfl_jj_rating_prediction_01', 'U') is not null
@@ -6,12 +10,13 @@ drop table temp.nfl_jj_rating_prediction_01;
 if object_id('temp.nfl_jj_rating_prediction_02', 'U') is not null
 drop table temp.nfl_jj_rating_prediction_02; 
 if object_id('jjprx.nfl_actual_and_trend', 'U') is not null
-drop table jjprx.nfl_jj_actual_and_trend; 
+drop table jjprx.nfl_actual_and_trend; 
 
 
 -- add player game id
 select 
 a.pfr_player_id
+, a.pfr_game_id
 , player_game_id = row_number() over(partition by a.pfr_player_id order by a.season asc, a.week asc)
 , a.receiving_rat
 into temp.nfl_jj_rating_prediction_01
@@ -39,15 +44,17 @@ select
     , t_01.player_game_id
     , t_01.receiving_rat
     */
-    t_00.player_game_id
+    t_00.pfr_player_id
+    , t_00.pfr_game_id
+    , t_00.player_game_id
     , actual = max(t_00.receiving_rat)
     , instant_trend = avg(t_01.receiving_rat)
     , short_trend = avg(t_02.receiving_rat)
     , medium_trend = avg(t_03.receiving_rat)
     , intermediate_trend = avg(t_04.receiving_rat)
     , long_trend = avg(t_05.receiving_rat)
-into jjprx.nfl_jj_actual_and_trend
-from (select distinct pfr_player_id, player_game_id, receiving_rat, instant, short, medium, intermediate, long from temp.nfl_jj_rating_prediction_02) t_00 
+into jjprx.nfl_actual_and_trend
+from (select distinct pfr_player_id, pfr_game_id, player_game_id, receiving_rat, instant, short, medium, intermediate, long from temp.nfl_jj_rating_prediction_02) t_00 
 left join temp.nfl_jj_rating_prediction_02 t_01 on t_00.player_game_id < (t_01.player_game_id + coalesce(t_00.instant, 2) + 1) and t_00.player_game_id >= (t_01.player_game_id + 1)
 left join temp.nfl_jj_rating_prediction_02 t_02 on t_00.player_game_id < (t_02.player_game_id + coalesce(t_00.short, 4) + 1) and t_00.player_game_id >= (t_02.player_game_id + 1)
 left join temp.nfl_jj_rating_prediction_02 t_03 on t_00.player_game_id < (t_03.player_game_id + coalesce(t_00.medium, 8) + 1) and t_00.player_game_id >= (t_03.player_game_id + 1)
@@ -58,7 +65,9 @@ where 1 = 1
     and t_00.player_game_id > 5
     -- and t_00.player_game_id = 24
 group by 
-    t_00.player_game_id
+    t_00.pfr_player_id
+    , t_00.pfr_game_id
+    , t_00.player_game_id
 
 
 -- postdrop tables
@@ -67,4 +76,5 @@ drop table temp.nfl_jj_rating_prediction_01;
 if object_id('temp.nfl_jj_rating_prediction_02', 'U') is not null
 drop table temp.nfl_jj_rating_prediction_02; 
 -- if object_id('jjprx.nfl_actual_and_trend', 'U') is not null
--- drop table jjprx.nfl_jj_actual_and_trend; 
+-- drop table jjprx.nfl_actual_and_trend; 
+GO
