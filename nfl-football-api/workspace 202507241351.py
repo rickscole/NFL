4ab835913d43bdbb1d8993dfc3ee4f234abf4364
rickscole <ricksaubercole@gmail.com>
2025-07-24 13:51:36 -0400

    Create workspace 202507241351.py

---
+++ nfl-football-api/workspace 202507241351.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 24 12:20:23 2025
+
+here we have found the desired endpoint
+
+@author: rcole
+"""
+
+
+
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("nfl-football-api.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "16b08d3c3emshba4d425ac4bc9a9p1e6e4cjsn0b14d9c5bece",
+    'x-rapidapi-host': "nfl-football-api.p.rapidapi.com"
+}
+
+conn.request("GET", "/nfl-ath-gamelog?id=4262921", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+my_json = data.decode('utf8').replace("'", '"')
+json_loads = json.loads(my_json)
+
+a = json_loads['seasonTypes']
+b = a[1]
+c = b['categories']
+d = c[0]
+e = d['events']
+
+
+receptions = e[0]['stats'][0]
+receiving_targets = e[0]['stats'][1]
+receiving_yards = e[0]['stats'][2]
+yards_per_reception = e[0]['stats'][3]
+receiving_touchdowns = e[0]['stats'][4]
+long_reception = e[0]['stats'][5]
+rushing_attempts = e[0]['stats'][6]
+rushing_yards = e[0]['stats'][7]
+yards_per_rush_attempt = e[0]['stats'][8]
+long_rushing = e[0]['stats'][9]
+rushing_touchdowns = e[0]['stats'][10]
+fumbles = e[0]['stats'][11]
+fumbles_lost = e[0]['stats'][12]
+fumbles_forced = e[0]['stats'][13]
+kicks_blocked = e[0]['stats'][14]
+
+
+
+#with open(r'/Users/rcole/Desktop/imacgration/lfn/texttxt.txt', "w") as f:
+#   json.dump(data, f)
