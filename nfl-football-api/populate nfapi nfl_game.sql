7b30c0b90169a8305bdb510e90f052f3117f3544
rickscole <ricksaubercole@gmail.com>
2025-08-07 16:09:23 -0400

    Update populate nfapi nfl_game.sql
    add logic for home and away team

--- nfl-football-api/populate nfapi nfl_game.sql
+++ nfl-football-api/populate nfapi nfl_game.sql
@@ -1,5 +1,17 @@
-insert into nfapi.nfl_game 
+insert into nfapi.nfl_game
 select 
 sysdatetime()
-, *
+, external_id 
+, season
+, home_team  = case 
+when charindex('@', matchup) > 0 then substring(matchup, charindex('@', matchup) + 2, len(matchup)) -- away
+when charindex('VS', matchup) > 0 then substring(matchup, charindex('VS', matchup) + 3, len(matchup)) -- home
+else 'other'
+end
+, away_team  = case 
+when charindex('@', matchup) > 0 then substring(matchup, 1, charindex('@', matchup) - 1) -- away
+when charindex('VS', matchup) > 0 then substring(matchup, 1, charindex('VS', matchup) - 2) -- home
+else 'other'
+end
 from temp.nfl_game
+
