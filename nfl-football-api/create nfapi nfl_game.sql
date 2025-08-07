869732a4511dbd7030c3b96dd3a0a7c6cf22fa16
rickscole <ricksaubercole@gmail.com>
2025-08-07 15:51:36 -0400

    Update create nfapi nfl_game.sql
    add home team and away team

--- nfl-football-api/create nfapi nfl_game.sql
+++ nfl-football-api/create nfapi nfl_game.sql
@@ -1,7 +1,18 @@
-create table nfapi.nfl_game 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [nfapi].[nfl_game](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[ts] [datetime2](7) NULL,
+	[external_id] [int] NULL,
+	[season] [smallint] NULL,
+	[home_team] [nvarchar](100) NULL,
+	[away_team] [nvarchar](100) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [nfapi].[nfl_game] ADD PRIMARY KEY CLUSTERED 
 (
-    id int primary key identity(1,1)
-    , ts datetime2(7)
-    , external_id [int] NULL
-    , [season] [smallint] NULL 
-)
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
