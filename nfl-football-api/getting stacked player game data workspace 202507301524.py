11e7698f9b8719831ccdf748e7b5527e98fb9962
rickscole <ricksaubercole@gmail.com>
2025-08-07 23:38:38 -0400

    Update getting stacked player game data workspace 202507301524.py
    previously, incorrect handling of players. now adding a loop for players.

--- nfl-football-api/getting stacked player game data workspace 202507301524.py
+++ nfl-football-api/getting stacked player game data workspace 202507301524.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 28 09:10:54 2025
-
-
-getting player ids by game id (for various seasons)
+Created on Thu Aug  7 23:22:39 2025
 
 @author: rcole
 """
+
 
 
 import http.client
@@ -26,7 +24,7 @@ headers = {
 
 
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2024.csv')
-#df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2023.csv')
+df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2023.csv')
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2022.csv')
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2021.csv')
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2020.csv')
@@ -36,10 +34,11 @@ headers = {
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2016.csv')
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2015.csv')
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2014.csv')
-df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2014 01.csv')
+#df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2014 01.csv')
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2013.csv')
 #df = pd.read_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi game ids 2013 01.csv')
 input_game_id_list = list(df['external_id'])
+#input_game_id_list = [401671651]
 
 
 player_id_list = []
@@ -73,21 +72,24 @@ for game_id in input_game_id_list:
         
             #if 'stats' in stat['athletes'][0]:
             if len(stat['athletes']) > 0:
-                player_id = stat['athletes'][0]['athlete']['id']
                 stat_type = stat['labels']
-                stat_value = stat['athletes'][0]['stats']
-                number_of_stats = len(stat_value)
                 
-                player_id_list.extend([player_id] * number_of_stats)
-                stat_type_list.extend(stat_type)
-                stat_value_list.extend(stat_value)
+                players = stat['athletes']
+                for player in players:
+                    player_id = player['athlete']['id']
+                    stat_value = player['stats']
+                    number_of_stats = len(stat_value)
+                
+                    player_id_list.extend([player_id] * number_of_stats)
+                    stat_type_list.extend(stat_type)
+                    stat_value_list.extend(stat_value)
     
-                #game_id_list = [game_id] * len(player_id_list)
-                game_id_list.extend([game_id] * number_of_stats)
+                    #game_id_list = [game_id] * len(player_id_list)
+                    game_id_list.extend([game_id] * number_of_stats)
 
 df_result = pd.DataFrame({'player_id' : player_id_list, 'game_id' : game_id_list, 'stat_type' : stat_type_list, 'stat_value' : stat_value_list})
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2024.csv', index = False)
-#df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2023.csv', index = False)
+df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2023.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2022.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2021.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2020.csv', index = False)
@@ -98,7 +100,9 @@ df_result = pd.DataFrame({'player_id' : player_id_list, 'game_id' : game_id_list
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2015.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2014.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2014 01.csv', index = False)
-df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2014 02.csv', index = False)
+#df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2014 02.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2013 01.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2013 02.csv', index = False)
 #df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/example nfapi player game stats 2013.csv', index = False)
+#df_result.to_csv(r'/Users/rcole/Desktop/imacgration/lfn/test.csv', index = False)
+
