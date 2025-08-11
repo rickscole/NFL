bf8a404111a81dc62789ccfc4cece83d6dc5c4d6
rickscole <ricksaubercole@gmail.com>
2025-08-10 21:26:57 -0400

    Update create nfapi nfl_game.sql
    add in season_type

--- nfl-football-api/create nfapi nfl_game.sql
+++ nfl-football-api/create nfapi nfl_game.sql
@@ -7,6 +7,7 @@ CREATE TABLE [nfapi].[nfl_game](
 	[ts] [datetime2](7) NULL,
 	[external_id] [int] NULL,
 	[season] [smallint] NULL,
+    season_type tinyint null,
 	[home_team] [nvarchar](100) NULL,
 	[away_team] [nvarchar](100) NULL
 ) ON [PRIMARY]
@@ -16,3 +17,5 @@ ALTER TABLE [nfapi].[nfl_game] ADD PRIMARY KEY CLUSTERED
 	[id] ASC
 )WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
+
+
