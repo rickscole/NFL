9aae059e45694a1014bee4a4694775071aa497fe
rickscole <ricksaubercole@gmail.com>
2025-09-02 13:22:01 -0400

    Update create nfapi nfl_player_game_stats_stacked.sql
    add stat category and adjust code

--- nfl-football-api/create nfapi nfl_player_game_stats_stacked.sql
+++ nfl-football-api/create nfapi nfl_player_game_stats_stacked.sql
@@ -13,6 +13,7 @@ a.player_id
 , a.game_id
 , a.player_name
 , a.stat_type
+, a.stat_category
 , value_01 = case 
 when a.stat_type in ('C/ATT', 'FG', 'XP') then substring(a.stat_value, 1, charindex('/', a.stat_value) - 1)
 when a.stat_type  = 'SACKS' then substring(a.stat_value, 1, charindex('-', a.stat_value) - 1) 
@@ -27,12 +28,16 @@ where 1 = 1
 and a.stat_type in ('C/ATT', 'FG', 'SACKS', 'XP')
 and try_convert(decimal(12, 6), a.stat_value) is null
 
-select e.player_id, e.game_id, e.player_name, e.stat_type, stat_value = try_convert(decimal(18, 6), e.stat_value) 
-into nfapi.nfl_player_game_stats_stacked from [temp].[nfl_nfapi_player_game_stats_stacked] e where e.stat_type not in ('C/ATT', 'FG', 'SACKS', 'XP') union all
-select d.* from (
-select a.player_id, a.game_id, a.player_name, stat_type = case when a.stat_type = 'C/ATT' then 'COMPS' when a.stat_type = 'SACKS' then 'SCKS' when a.stat_type = 'FG' then 'FGM' when a.stat_type = 'XP' then 'XPM' else null end, value = value_01 from temp.nfl_nfapi_player_game_stats_stacked_01 a
-union select b.player_id, b.game_id, b.player_name, stat_type = case when b.stat_type = 'C/ATT' then 'ATTS' when b.stat_type = 'SACKS' then 'YDSL' when b.stat_type = 'FG' then 'FGA' when b.stat_type = 'XP' then 'XPA' else null end, value = value_02 from temp.nfl_nfapi_player_game_stats_stacked_01 b
-union select c.player_id, c.game_id, c.player_name, stat_type = case when c.stat_type = 'C/ATT' then 'C/ATT' when c.stat_type = 'SACKS' then 'SACKS' when c.stat_type = 'FG' then 'FG' when c.stat_type = 'XP' then 'XP' else null end, value = case when value_02 = 0 then null else cast( value_01 as decimal(18, 6)) / cast( value_02 as decimal(18, 6)) end from temp.nfl_nfapi_player_game_stats_stacked_01 c
+
+select e.player_id, e.game_id, e.player_name, e.stat_category, e.stat_type, stat_value = try_convert(decimal(18, 6), e.stat_value) 
+into nfapi.nfl_player_game_stats_stacked from [temp].[nfl_nfapi_player_game_stats_stacked] e 
+where not(e.stat_type in ('C/ATT', 'FG', 'SACKS', 'XP') and try_convert(decimal(12, 6), e.stat_value) is null)
+union all
+select d.* from 
+(
+select a.player_id, a.game_id, a.player_name, a.stat_category, stat_type = case when a.stat_type = 'C/ATT' then 'COMPS' when a.stat_type = 'SACKS' then 'SCKS' when a.stat_type = 'FG' then 'FGM' when a.stat_type = 'XP' then 'XPM' else null end, value = value_01 from temp.nfl_nfapi_player_game_stats_stacked_01 a
+union select b.player_id, b.game_id, b.player_name, b.stat_category, stat_type = case when b.stat_type = 'C/ATT' then 'ATTS' when b.stat_type = 'SACKS' then 'YDSL' when b.stat_type = 'FG' then 'FGA' when b.stat_type = 'XP' then 'XPA' else null end, value = value_02 from temp.nfl_nfapi_player_game_stats_stacked_01 b
+union select c.player_id, c.game_id, c.player_name, c.stat_category, stat_type = case when c.stat_type = 'C/ATT' then 'C/ATT' when c.stat_type = 'SACKS' then 'SACKS' when c.stat_type = 'FG' then 'FG' when c.stat_type = 'XP' then 'XP' else null end, value = case when value_02 = 0 then null else cast( value_01 as decimal(18, 6)) / cast( value_02 as decimal(18, 6)) end from temp.nfl_nfapi_player_game_stats_stacked_01 c
 ) d where d.value is not null
 
 
@@ -41,3 +46,8 @@ union select c.player_id, c.game_id, c.player_name, stat_type = case when c.stat
 -- postdrop tables
 if object_id('temp.nfl_nfapi_player_game_stats_stacked_01', 'U') is not null
 drop table temp.nfl_nfapi_player_game_stats_stacked_01;
+
+
+
+
+
